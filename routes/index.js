8677cbb5855bb77b279494fab5e964192424046f
Truett Hilliard <truetthilliard@Truetts-MacBook-Pro.local>
2020-09-07 20:17:34 -0500

    fix site

--- routes/index.js
+++ routes/index.js
@@ -3,48 +3,50 @@ var router = express.Router();
 var User = require("../models/user");
 var passport = require("passport");
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
 	res.render("landing");
 });
 
 //=============
-//Auth ROutes
+//Auth Routes
 //=============
 //show register form
-router.get("/register", function(req, res){
+router.get("/register", function (req, res) {
 	res.render("register");
 });
 
 //sign up logic
-router.post("/register", function(req, res){
-	var newUser = new User({username:req.body.username});
-	User.register(newUser, req.body.password, function(err, user){
-		if(err){
+router.post("/register", function (req, res) {
+	var newUser = new User({ username: req.body.username });
+	User.register(newUser, req.body.password, function (err, user) {
+		if (err) {
 			req.flash("error", err.message);
 			res.render("register");
 		}
-		passport.authenticate("local")(req, res, function(){
-			req.flash("success", "Welcome to YelpCamp " + user.username);
+		passport.authenticate("local")(req, res, function () {
+			req.flash("success", "Welcome to Texas Parks " + user.username);
 			res.redirect("/campgrounds");
 		});
 	});
 });
 
 //show login form
-router.get("/login", function(req, res){
-	res.render("login");
+router.get("/login", function (req, res) {
+	res.render("login", { page: 'login' });
 });
 
-router.post("/login", passport.authenticate("local", 
+//login logic
+router.post("/login", passport.authenticate("local",
 	{
 		successRedirect: "/campgrounds",
-		failureRedirect: "/login"
-	}), function(req, res){
-	req.flash("success", "Signed in as " + user.username);
-});
+		failureRedirect: "/login",
+		failureFlash: true,
+		successFlash: 'Welcome to Texas Parks!'
+	}), function (req, res) {
+	});
 
-//Logout ROute
-router.get("/logout", function(req, res){
+//Logout Route
+router.get("/logout", function (req, res) {
 	req.logout();
 	req.flash("success", "Logged you out!")
 	res.redirect("/campgrounds");
